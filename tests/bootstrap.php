0a49941e29c975564937054e0be86f419d1a5f57
Sebastian Andreoletti <andreole@ar.ibm.com>
2022-03-29 09:26:04 -0300

    update  tests/bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,8 +5,4 @@
  * Add additional configuration/setup your application needs when running
  * unit tests in this file.
  */
-require dirname(__DIR__) . '/vendor/autoload.php';
-
-require dirname(__DIR__) . '/config/bootstrap.php';
-
 $_SERVER['PHP_SELF'] = '/';
